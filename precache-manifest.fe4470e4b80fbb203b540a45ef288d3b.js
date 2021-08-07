2da4097f1ea456582c19a4dbaaa9f15e8764f1dd
INValentin <57767759+INValentin@users.noreply.github.com>
2021-08-08 01:17:14 +0200

    Add files via upload

---
+++ precache-manifest.fe4470e4b80fbb203b540a45ef288d3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89eb87525e057a8d401b33e6e45729fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "363dfee4c75ef0566446",
+    "url": "/static/css/main.8c46fa56.chunk.css"
+  },
+  {
+    "revision": "72dfc2617ac8a1b09ae3",
+    "url": "/static/js/2.e3dd2d83.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e3dd2d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363dfee4c75ef0566446",
+    "url": "/static/js/main.afd87c03.chunk.js"
+  },
+  {
+    "revision": "ce7b350515357ce83319",
+    "url": "/static/js/runtime-main.b56792ae.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
